3b8cd47d6995911fb33bbc0015fdfafe04af2989
MRNobody183 <Rajamohib9@gmail.com>
2023-03-03 13:04:56 +0500

    adding the token validation

--- pages/passwordPage.tsx
+++ pages/passwordPage.tsx
@@ -12,30 +12,36 @@ function HomePage() {
     (state: RootState) => state.signupState.signUpState.confirmation
   );
   const router = useRouter();
+  const { email, token } = router.query;
   /*   useEffect(()=>{
-    const { token } = router.query;
+   
     const response=axios.get(`http://localhost:8000/user/verify?token=${token}`)
     console.log('verified user',response)
   },[]) */
-  return (
-    <>
-      {isAccountCreated ? (
-        <div className="w-screen bg-[#F6F8FD]">
-          <div className="w-full">
-            <PortfolioMonitoring />
+  if (token == "helog")
+      return (
+      <>
+        {isAccountCreated ? (
+          <div className="w-screen bg-[#F6F8FD]">
+            <div className="w-full">
+              <PortfolioMonitoring />
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="flex h-screen ">
-          <div className="flex bg-[#08101f] text-white w-1/3">
-            <SetPassword />
+        ) : (
+          <div className="flex h-screen ">
+            <div className="flex bg-[#08101f] text-white w-1/3">
+              <SetPassword />
+            </div>
+            <div className="flex justify-center bg-[#171f2d] text-white w-full h-full">
+              <Home />
+            </div>
           </div>
-          <div className="flex justify-center bg-[#171f2d] text-white w-full h-full">
-            <Home />
-          </div>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    );
+   else
+   return(<div>
+    phut yha say
+   </div>) 
 }
 export default HomePage;
