b755128bbab840f43684a678b882f48b6bef5311
MRNobody183 <Rajamohib9@gmail.com>
2023-03-03 16:01:57 +0500

    added validation for password page load

--- pages/passwordPage.tsx
+++ pages/passwordPage.tsx
@@ -6,20 +6,30 @@ import { useRouter } from "next/router";
 import PortfolioMonitoring from "../components/portfolioMonitor/portfolioSetup";
 import SetPassword from "../components/homePage/authComponents/signup/SetPassword";
 import Home from "../components/homePage/Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomePage() {
+  const [istoken, setToken] = useState<boolean>(false);
   const isAccountCreated = useSelector(
     (state: RootState) => state.signupState.signUpState.confirmation
   );
   const router = useRouter();
   const { email, token } = router.query;
-  /*   useEffect(()=>{
-   
-    const response=axios.get(`http://localhost:8000/user/verify?token=${token}`)
-    console.log('verified user',response)
-  },[]) */
-  if (token == "helog")
-      return (
+  const checkdata = async () => {
+    try {
+      console.log("verified user", email, token);
+      const response = await axios.get(
+        `http://localhost:8000/user/verify-token?email=${email}&token=${token}`
+      );
+      setToken(true);
+    } catch (error) {
+      setToken(false);
+    }
+  };
+  checkdata();
+  if (istoken)
+    return (
       <>
         {isAccountCreated ? (
           <div className="w-screen bg-[#F6F8FD]">
@@ -39,9 +49,6 @@ function HomePage() {
         )}
       </>
     );
-   else
-   return(<div>
-    phut yha say
-   </div>) 
+  else return <div>phut yha say</div>;
 }
 export default HomePage;
