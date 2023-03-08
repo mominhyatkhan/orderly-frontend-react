435591ff2d5d274a1aa119f915f42cc5de89b218
MRNobody183 <Rajamohib9@gmail.com>
2023-03-08 12:50:38 +0500

    updated the validation of the password

--- components/homePage/authComponents/signup/SetPassword.tsx
+++ components/homePage/authComponents/signup/SetPassword.tsx
@@ -4,76 +4,78 @@ import { setPassword } from "../../../../pages/slices/signupSlice";
 import PasswordValidator from "password-validator";
 import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
-const schema = new PasswordValidator();
-schema
-  .is()
-  .min(8) // Minimum length of 8 characters
-  .has()
-  .uppercase(4) // Minimum of 4 uppercase letters
-  .has()
-  .lowercase(4) // Minimum of 4 lowercase letters
-  .has()
-  .digits(4) // Minimum of 4 digits
-  .has()
-  .symbols(); // Requires symbols
-
-function validatePassword(password: string): boolean {
-  const validationResult = schema.validate(password, {
-    list: true,
-  }) as string[];
-  return validationResult.length === 0;
-}
 
 function SetPassword() {
   const router = useRouter();
-  const {email,token}=router.query
+  const { email, token } = router.query;
 
   const [password, setpassword] = useState<string>("");
-  const [passwordError, setPasswordError] = useState("");
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
+  const [hasFourNonConsecutiveNumbers, setHasFourNonConsecutiveNumbers] =
+    useState(false);
+  const [hasFourCapitalAndLowerCase, setHasFourCapitalAndLowerCase] =
+    useState(false);
+  const [hasSymbol, setHasSymbol] = useState(false);
 
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value;
+    setpassword(newPassword);
+    setHasFourNonConsecutiveNumbers(
+      /(?!(?:\d|[a-z])\1{3})\d{4}/i.test(newPassword)
+    );
+    setHasFourCapitalAndLowerCase(
+      /^(?=.*[a-z])(?=.*[A-Z]).{4,}$/g.test(newPassword)
+    );
+    setHasSymbol(/[!@#$%^&*(),.?":{}|<>]/.test(newPassword));
+  };
   function handleTogglePassword() {
     setShowPassword(!showPassword);
   }
-  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const newPassword = event.target.value;
-    setPassword(newPassword);
-    console.log(newPassword);
-    if (!validatePassword(newPassword)) {
-      setPasswordError(
-        "Minimum 4 non-consecutive numbers  Mininum 4 capital & lowercase letters"
-      );
-    } else {
-      setPasswordError("");
-    }
-  }
-const setPasswordDb=()=>{
-  
-  dispatch(setPassword({ password }))
 
-}
+  const setPasswordDb = () => {
+    dispatch(setPassword({ password }));
+  };
   return (
     <div className="flex w-full">
       <form
         onSubmit={(e) => {
           e.preventDefault(),
-            !passwordError && setPasswordDb;
+            hasFourNonConsecutiveNumbers &&
+              hasFourCapitalAndLowerCase &&
+              hasSymbol &&
+              setPasswordDb;
         }}
         className="flex w-full"
       >
         <div className="flex justify-center flex-col w-full ">
           <div className="self-center items-center flex flex-col  gap-y-6">
             <label className="flex self-center text-4xl">Welcome</label>
-            <p className="w-[394px] text-lg text-center">Set the password to protect your account</p>
+            <p className="w-[394px] text-lg text-center">
+              Set the password to protect your account
+            </p>
             <div className="flex flex-row bg-white focus rounded-lg h-[56px] w-[320px]">
-              <input
-                type={showPassword ? "text" : "password"}
+              {/*  <input
+                
                 onChange={handlePasswordChange}
                 id="password"
                 className="bg-white w-full text-black text-sm rounded-lg focus:outline-none  block p-2.5 placeholder-black dark:text-black "
                 placeholder="Password"
                 required
+              /> */}
+              <input
+                type={showPassword ? "text" : "password"}
+                className={`${
+                  !hasFourNonConsecutiveNumbers ||
+                  !hasFourCapitalAndLowerCase ||
+                  !hasSymbol
+                    ? "border-red-500"
+                    : "border-gray-400"
+                } bg-white w-full text-black text-sm rounded-lg focus:outline-none  block p-2.5 placeholder-black dark:text-black`}
+                id="password"
+                placeholder="******************"
+                value={password}
+                onChange={handlePasswordChange}
               />
               <button
                 className=" inset-y-0 right-0 flex items-center px-3 text-gray-600 focus:outline-none justify-end"
@@ -93,16 +95,39 @@ const setPasswordDb=()=>{
             >
               Let's Start
             </button>
-            {passwordError && (
-              <div className="justify-center flex flex-col text-green-500">
-                <label>Minimum 4 non-consecutive numbers</label>
-                <label>Mininum 4 capital & lowercase letters</label>
-                <label className="text-red-500 self-center">Symbols</label>
-                <label className="text-red-500 mt-5 italic">
-                  Your password is not strong enough yet
-                </label>
-              </div>
-            )}
+
+            <div className="justify-center flex flex-col text-green-500 space-y-2">
+              {/*   <label>Minimum 4 non-consecutive numbers</label>
+              <label>Mininum 4 capital & lowercase letters</label>
+              <label className="text-red-500 self-center">Symbols</label>
+              <label className="text-red-500 mt-5 italic">
+                Your password is not strong enough yet
+              </label> */}
+              <p
+                className={`text-sm ${
+                  hasFourNonConsecutiveNumbers
+                    ? "text-green-500"
+                    : "text-red-500"
+                }`}
+              >
+                Minimum 4 non-consecutive numbers
+              </p>
+              <p
+                className={`text-sm ${
+                  hasFourCapitalAndLowerCase ? "text-green-500" : "text-red-500"
+                }`}
+              >
+                Minimum 4 capital & lowercase letters
+              </p>
+              <p
+                className={`text-sm self-center ${
+                  hasSymbol ? "text-green-500" : "text-red-500"
+                }`}
+              >
+                Symbols
+              </p>
+            </div>
+
             <div className="flex gap-1 justify-center">
               <label>Have an account?</label>
               <a className="font-bold">Log In</a>
